f4ec9066f4080e56e1ff0b1bfa42b6c048ef0aca
Pinguin <maxsunkovsky@gmail.com>
2023-12-20 00:23:41 +0100

    Found a solution for multiple tests (cargo test [specifier])

--- src/bin/two_sum.rs
+++ src/bin/two_sum.rs
@@ -19,19 +19,19 @@ fn main() {
 #[cfg(test)]
 
 #[test]
-fn test_case_1() {
+fn test_two_sum_case_1() {
     let nums = vec![2, 7, 11, 15];
     let target = 9;
     assert_eq!(Solution::two_sum(nums, target), vec![0, 1]);
 }
 #[test]
-fn test_case_2() {
+fn test_two_sum_case_2() {
     let nums = vec![3, 2, 4];
     let target = 6;
     assert_eq!(Solution::two_sum(nums, target), vec![1, 2]);
 }
 #[test]
-fn test_case_3() {
+fn test_two_sum_case_3() {
     let nums = vec![3, 3];
     let target = 6;
     assert_eq!(Solution::two_sum(nums, target), vec![0, 1]);
