0a737957a566777ff8e86d6dabd0bcee5da33cb9
Pinguin <maxsunkovsky@gmail.com>
2024-07-02 22:19:36 +0200

    small progress. little time

--- src/bin/palindrome_number.rs
+++ src/bin/palindrome_number.rs
@@ -3,14 +3,19 @@ pub struct Solution;
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
         let input = x.to_string();
-        let rev_input:String = input.chars().rev().collect();
+        let rev_input: String = input.chars().rev().collect();
         if input == rev_input {
-            return true
+            return true;
         }
         false
     }
 }
 
-fn main(){
-    println!("Palindrome Number");
+fn main() {
+    let x: i32 = 1401;
+    if Solution::is_palindrome(x) {
+        println!("Palindrome!");
+    } else {
+        print!("Not a palindrome");
+    }
 }
--- src/bin/roman_to_integer.rs
+++ src/bin/roman_to_integer.rs
@@ -1,14 +1,28 @@
 pub struct Solution;
 
 impl Solution {
-
     pub fn roman_to_int(s: String) -> i32 {
-        let result:i32 = 0;
-
+        let mut result: i32 = 0;
+        let chars: Vec<char> = s.chars().collect(); // Vector to iterate over all chars
+        let mut numbers: Vec<i32> = Vec::new(); // Vector to store the numeral values
+        for element in chars {
+            match element {
+                'I' => numbers.push(1),     // can be placed before V and X
+                'V' => numbers.push(5),
+                'X' => numbers.push(10),    // can be placed before V and X
+                'L' => numbers.push(50),
+                'C' => numbers.push(100),   // can be placed before V and X
+                'D' => numbers.push(500),
+                'M' => numbers.push(1000),
+                _ => {} // ignore every other possible char
+            }
+        }
         result
     }
 }
 
 fn main() {
-    println!("Roman to Integer");
+    let s = "LVIII".to_string();
+    let result = Solution::roman_to_int(s);
+    println!("Your number is: {}", result);
 }
