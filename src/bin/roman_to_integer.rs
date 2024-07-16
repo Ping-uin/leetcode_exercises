1ce91a8e1dcf73fb10581e8ca74b32359e09aae9
Pinguin <maxsunkovsky@gmail.com>
2024-07-16 23:29:39 +0200

    roman_to_integer done

--- src/bin/roman_to_integer.rs
+++ src/bin/roman_to_integer.rs
@@ -1,20 +1,37 @@
+use std::collections::HashMap;
+
 pub struct Solution;
 
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        let mut result: i32 = 0;
-        let chars: Vec<char> = s.chars().collect(); // Vector to iterate over all chars
-        let mut numbers: Vec<i32> = Vec::new(); // Vector to store the numeral values
-        for element in chars {
-            match element {
-                'I' => numbers.push(1),     // can be placed before V and X
-                'V' => numbers.push(5),
-                'X' => numbers.push(10),    // can be placed before V and X
-                'L' => numbers.push(50),
-                'C' => numbers.push(100),   // can be placed before V and X
-                'D' => numbers.push(500),
-                'M' => numbers.push(1000),
-                _ => {} // ignore every other possible char
+        // Assign each possible char its numeral value
+        let mut roman_values: HashMap<char, i32> = HashMap::new();
+        roman_values.insert('I', 1); // can be placed before V and X
+        roman_values.insert('V', 5);
+        roman_values.insert('X', 10); // can be placed before L and C
+        roman_values.insert('L', 50);
+        roman_values.insert('C', 100); // can be placed before D and M
+        roman_values.insert('D', 500);
+        roman_values.insert('M', 1000);
+
+        let mut result = 0;
+        let mut index = 0;
+        let chars: Vec<char> = s.chars().collect(); // Extract all chars from the input string
+
+        // Iterate over each char in string s
+        while index < s.len() {
+            let mut curr_num = roman_values.get(&chars[index]).unwrap();
+            let mut next_num = if index < s.len() - 1 {
+                roman_values.get(&chars[index + 1]).unwrap()
+            } else {
+                &0 // that works??
+            };
+            if curr_num < next_num {
+                result += next_num - curr_num;
+                index += 2;
+            } else {
+                result += curr_num;
+                index += 1;
             }
         }
         result
@@ -22,7 +39,7 @@ impl Solution {
 }
 
 fn main() {
-    let s = "LVIII".to_string();
+    let s = "MCMXCIV".to_string();
     let result = Solution::roman_to_int(s);
     println!("Your number is: {}", result);
 }
