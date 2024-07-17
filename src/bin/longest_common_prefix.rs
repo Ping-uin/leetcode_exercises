e576ec9609a8463f30f10d9c71d4e7ca8c2e9241
Pinguin <maxsunkovsky@gmail.com>
2024-07-18 00:30:06 +0200

    longest common prefix almost working; index problem I think

--- src/bin/longest_common_prefix.rs
+++ src/bin/longest_common_prefix.rs
@@ -1,20 +1,37 @@
-
-
 pub struct Solution;
 
 impl Solution {
     fn find_longest_common_prefix(strs: Vec<String>) -> String {
-        let result = "".to_string();
-
+        let mut result = "".to_string();
+        let comparator = &strs[0].chars().collect::<Vec<_>>();
+        let mut i = 0;
+        let mut j = 0;
+        while i < strs.len() {
+            for char in strs[i].chars() {
+                if char == comparator[j] {
+                    result.push(char);
+                    if j < strs.len() - 1 {
+                        j += 1;
+                    }
+                } else {
+                    break;
+                }
+            }
+            i += 1;
+        }
         result
     }
 }
 
 pub fn main() {
-    let mut strs: Vec<String> = Vec::new(); 
-    strs.push("flower".to_string());
-    strs.push("flow".to_string());
-    strs.push("flight".to_string());
-    let prefix = Solution::find_longest_common_prefix(strs);
-    println!("Longest common prefix is {}", prefix);
+    let mut strs: Vec<String> = vec![
+        "flower".to_string(),
+        "flow".to_string(),
+        "flight".to_string()
+    ];
+    let mut prefix = Solution::find_longest_common_prefix(strs);
+    println!("First longest common prefix is {}", prefix);
+    strs = vec!["dog".to_string(), "racecar".to_string(), "car".to_string()];
+    prefix = Solution::find_longest_common_prefix(strs);
+    println!("First longest common prefix is {}", prefix);
 }
