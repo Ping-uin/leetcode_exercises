0fd5a2290cbdedb0361158ce41d80dbddf2f56a1
Pinguin <maxsunkovsky@gmail.com>
2024-07-23 22:30:44 +0200

    valid parentheses prepared

---
+++ src/bin/valid_parentheses.rs
@@ -0,0 +1,14 @@
+pub struct Solution;
+
+impl Solution {
+    fn is_valid(s: String) -> bool {
+        let mut result = true;
+
+        result
+    }
+}
+
+pub fn main() {
+    let s = "()[]{}".to_string();
+    println!("{}",Solution::is_valid(s));
+}
